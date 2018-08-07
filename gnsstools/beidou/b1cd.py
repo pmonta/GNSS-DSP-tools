1150484b69422299c69b7f41750af5e759684085
Peter Monta <pmonta@gmail.com>
2018-08-07 10:22:07 -0700

    add BeiDou B1Cd codes, acquisition, and tracking

---
+++ gnsstools/beidou/b1cd.py
@@ -0,0 +1,91 @@
+# Beidou B1Cd code construction
+#
+# Copyright 2018 Peter Monta
+
+import numpy as np
+from sympy.ntheory import legendre_symbol
+
+chip_rate = 1023000
+code_length = 10230
+
+b1cd_params = {
+   1: (2678,699),    2: (4802,694),    3: (958,7318),    4: (859,2127),
+   5: (3843,715),    6: (2232,6682),   7: (124,7850),    8: (4352,5495),
+   9: (1816,1162),  10: (1126,7682),  11: (1860,6792),  12: (4800,9973),
+  13: (2267,6596),  14: (424,2092),   15: (4192,19),    16: (4333,10151),
+  17: (2656,6297),  18: (4148,5766),  19: (243,2359),   20: (1330,7136),
+  21: (1593,1706),  22: (1470,2128),  23: (882,6827),   24: (3202,693),
+  25: (5095,9729),  26: (2546,1620),  27: (1733,6805),  28: (4795,534),
+  29: (4577,712),   30: (1627,1929),  31: (3638,5355),  32: (2553,6139),
+  33: (3646,6339),  34: (1087,1470),  35: (1843,6867),  36: (216,7851),
+  37: (2245,1162),  38: (726,7659),   39: (1966,1156),  40: (670,2672),
+  41: (4130,6043),  42: (53,2862),    43: (4830,180),   44: (182,2663),
+  45: (2181,6940),  46: (2006,1645),  47: (1080,1582),  48: (2288,951),
+  49: (2027,6878),  50: (271,7701),   51: (915,1823),   52: (497,2391),
+  53: (139,2606),   54: (3693,822),   55: (2054,6403),  56: (4342,239),
+  57: (3342,442),   58: (2592,6769),  59: (1007,2560),  60: (310,2502),
+  61: (4203,5072),  62: (455,7268),   63: (4318,341),
+}
+
+N = 10243
+L = np.array([legendre_symbol(i,N) for i in range(N)])
+L[L==-1] = 0
+L[0] = 0
+
+def b1cd(prn):
+  w,p = b1cd_params[prn]
+  W = np.array([L[k]^L[(k+w)%N] for k in range(N)])
+  c = np.array([W[(n+p-1)%N] for n in range(code_length)])
+  return c
+
+codes = {}
+
+def b1cd_code(prn):
+  if prn not in codes:
+    codes[prn] = b1cd(prn)
+  return codes[prn]
+
+def code(prn,chips,frac,incr,n):
+  c = b1cd_code(prn)
+  idx = (chips%code_length) + frac + incr*np.arange(n)
+  idx = np.floor(idx).astype('int')
+  idx = np.mod(idx,code_length)
+  x = c[idx]
+  return 1.0 - 2.0*x
+
+boc11 = np.array([1.0,-1.0])
+
+try:
+  from numba import jit
+except:
+  def jit(**kwargs):
+    return lambda x: x
+
+@jit(nopython=True)
+def correlate(x,prn,chips,frac,incr,c,boc11):
+  n = len(x)
+  p = 0.0j
+  cp = (chips+frac)%code_length
+  bp = (2*(chips+frac))%2
+  for i in range(n):
+    boc = boc11[int(bp)]
+    p += x[i]*(1.0-2.0*c[int(cp)])*boc
+    cp = (cp+incr)%code_length
+    bp = (bp+2*incr)%2
+  return p
+
+# test
+
+def chips2octal(x):
+  s = ''
+  for i in range(len(x)/3):
+    d = 4*x[3*i] + 2*x[3*i+1] + x[3*i+2]
+    s = s + '%o'%d
+  return s
+
+if __name__=='__main__':
+  for prn in range(1,64):
+    c = b1cd_code(prn)
+    s1 = chips2octal(c[0:24])
+    s2 = chips2octal(c[-24:])
+    print("%d %s %s"%(prn,s1,s2))
---
+++ track-beidou-b1cd.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+
+import sys
+import numpy as np
+
+import gnsstools.beidou.b1cd as b1cd
+import gnsstools.nco as nco
+import gnsstools.io as io
+import gnsstools.discriminator as discriminator
+
+class tracking_state:
+  def __init__(self,fs,prn,code_p,code_f,code_i,carrier_p,carrier_f,carrier_i,mode):
+    self.fs = fs
+    self.prn = prn
+    self.code_p = code_p
+    self.code_f = code_f
+    self.code_i = code_i
+    self.carrier_p = carrier_p
+    self.carrier_f = carrier_f
+    self.carrier_i = carrier_i
+    self.mode = mode
+    self.prompt1 = 0 + 0*(1j)
+    self.carrier_e1 = 0
+    self.code_e1 = 0
+    self.eml = 0
+
+# tracking loops
+
+def track(x,s):
+  n = len(x)
+  fs = s.fs
+
+  nco.mix(x,-s.carrier_f/fs, s.carrier_p)
+  s.carrier_p = s.carrier_p - n*s.carrier_f/fs
+  s.carrier_p = np.mod(s.carrier_p,1)
+
+  cf = (s.code_f+s.carrier_f/1540.0)/fs
+
+  p_early = b1cd.correlate(x, s.prn, 0, s.code_p-0.2, cf, b1cd.b1cd_code(prn), b1cd.boc11)
+  p_prompt = b1cd.correlate(x, s.prn, 0, s.code_p, cf, b1cd.b1cd_code(prn), b1cd.boc11)
+  p_late = b1cd.correlate(x, s.prn, 0, s.code_p+0.2, cf, b1cd.b1cd_code(prn), b1cd.boc11)
+
+  if s.mode=='FLL_WIDE':
+    fll_k = 2.0
+    a = p_prompt
+    b = s.prompt1
+    e = discriminator.fll_atan(a,b)
+    s.carrier_f = s.carrier_f + fll_k*e
+    s.prompt1 = p_prompt
+  elif s.mode=='FLL_NARROW':
+    fll_k = 0.6
+    a = p_prompt
+    b = s.prompt1
+    e = discriminator.fll_atan(a,b)
+    s.carrier_f = s.carrier_f + fll_k*e
+    s.prompt1 = p_prompt
+  elif s.mode=='PLL':
+    pll_k1 = 0.1
+    pll_k2 = 5.0
+    e = discriminator.pll_costas(p_prompt)
+    e1 = s.carrier_e1
+    s.carrier_f = s.carrier_f + pll_k1*e + pll_k2*(e-e1)
+    s.carrier_e1 = e
+
+# code loop
+
+  dll_k1 = 0.00002
+  dll_k2 = 0.2
+  s.early = np.absolute(p_early)
+  s.prompt = np.absolute(p_prompt)
+  s.late = np.absolute(p_late)
+  if (s.late+s.early)==0:
+    e = 0
+  else:
+    e = (s.late-s.early)/(s.late+s.early)
+  s.eml = e
+  e1 = s.code_e1
+  s.code_f = s.code_f + dll_k1*e + dll_k2*(e-e1)
+  s.code_e1 = e
+
+  s.code_p = s.code_p + n*cf
+  s.code_p = np.mod(s.code_p,b1cd.code_length)
+
+  return p_prompt,s
+
+#
+# main program
+#
+
+# parse command-line arguments
+# example:
+#   ./track-beidou-b1cd.py /dev/stdin 69984000 -9334875 12 -350 1274.2
+
+filename = sys.argv[1]             # input data, raw file, i/q interleaved, 8 bit signed (two's complement)
+fs = float(sys.argv[2])            # sampling rate, Hz
+coffset = float(sys.argv[3])       # offset to B1C carrier, Hz (positive or negative)
+prn = int(sys.argv[4])             # PRN code
+doppler = float(sys.argv[5])       # initial doppler estimate from acquisition
+code_offset = float(sys.argv[6])   # initial code offset from acquisition
+
+fp = open(filename,"rb")
+
+n = int(fs*0.01*((b1cd.code_length-code_offset)/b1cd.code_length))  # align with 10 ms code boundary
+x = io.get_samples_complex(fp,n)
+code_offset += n*100.0*b1cd.code_length/fs
+
+s = tracking_state(fs=fs, prn=prn,                    # initialize tracking state
+  code_p=code_offset, code_f=b1cd.chip_rate, code_i=0,
+  carrier_p=0, carrier_f=doppler, carrier_i=0,
+  mode='PLL')
+
+block = 0
+coffset_phase = 0.0
+
+while True:
+  if s.code_p<b1cd.code_length/2:
+    n = int(fs*0.01*(b1cd.code_length-s.code_p)/b1cd.code_length)
+  else:
+    n = int(fs*0.01*(2*b1cd.code_length-s.code_p)/b1cd.code_length)
+
+  x = io.get_samples_complex(fp,n)
+  if x is None:
+    break
+
+  nco.mix(x,-coffset/fs,coffset_phase)
+  coffset_phase = coffset_phase - n*coffset/fs
+  coffset_phase = np.mod(coffset_phase,1)
+
+  for j in range(10):
+    a,b = int(j*n/10),int((j+1)*n/10)
+    p_prompt,s = track(x[a:b],s)
+    vars = block, np.real(p_prompt), np.imag(p_prompt), s.carrier_f, s.code_f-b1cd.chip_rate, (180/np.pi)*np.angle(p_prompt), s.early, s.prompt, s.late
+    print('%d %f %f %f %f %f %f %f %f' % vars)
+
+    block = block + 1
+#    if (block%100)==0:
+#      sys.stderr.write("%d\n"%block)
+#    if block==500:
+#      s.mode = 'FLL_NARROW'
+#    if block==1000:
+#      s.mode = 'PLL'
