711a7f61628478103f1ea9a93f0a7a608a6917f0
Peter Monta <pmonta@gmail.com>
2025-08-09 13:23:21 -0400

    acquisition and tracking for Xona X1

---
+++ acquire-xona-x1.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+
+import optparse
+
+import numpy as np
+import scipy.signal
+import scipy.fftpack as fft
+
+import gnsstools.xona.x1p as x1p
+import gnsstools.nco as nco
+import gnsstools.io as io
+import gnsstools.util as util
+
+#
+# Acquisition search
+#
+
+def search(x,prn,doppler_search,ms):
+  fs = 4096000.0
+  n = 4096                                         # 1 ms coherent integration
+  doppler_min, doppler_max, doppler_incr = doppler_search
+  incr = float(x1p.code_length)/n
+  c = x1p.code(prn,0,0,incr,n)                      # obtain samples of the C/A code
+  c = fft.fft(c)
+  m_metric,m_code,m_doppler = 0,0,0
+  for doppler in np.arange(doppler_min,doppler_max,doppler_incr):        # doppler bins
+    q = np.zeros(n)
+    w = nco.nco(-doppler/fs,0,n)
+    for block in range(ms):                        # incoherent sums
+      b = x[(block*n):((block+1)*n)]
+      b = b*w
+      r = fft.ifft(c*np.conj(fft.fft(b)))
+      q = q + np.absolute(r)
+    idx = np.argmax(q)
+    metric = q[idx]/np.mean(q)
+    if metric>m_metric:
+      m_metric = metric
+      m_code = x1p.code_length*(float(idx)/n)
+      m_doppler = doppler
+  return m_metric,m_code,m_doppler
+
+#
+# main program
+#
+
+parser = optparse.OptionParser(usage="""acquire-xona-x1.py [options] input_filename sample_rate carrier_offset
+
+Acquire Xona X1 signals
+
+Examples:
+  Acquire Xona using standard input with sample rate 69.984 MHz and carrier offset +8.567625 MHz:
+    acquire-xona-x1.py /dev/stdin 69984000 8567625
+  Acquire Xona with a custom doppler search grid and integration time of 20 ms:
+    acquire-xona-x1.py --doppler-search -50000,50000,500 --time 20 /dev/stdin 69984000 8567625
+  Acquire Xona from raw sample file "recording.iq":
+    acquire-xona-x1.py recording.iq 69984000 8567625
+
+Arguments:
+  input_filename    input data file, i/q interleaved, 8 bit signed
+  sample_rate       sampling rate in Hz
+  carrier_offset    offset to X1 carrier in Hz (positive or negative)""")
+
+parser.disable_interspersed_args()
+
+parser.add_option("--prn", default="0", help="PRNs to search, e.g. 1,3,7-14,31 (default %default)")
+parser.add_option("--doppler-search", metavar="MIN,MAX,INCR", default="-50000,50000,200", help="Doppler search grid: min,max,increment (default %default)")
+parser.add_option("--time", type="int", default=80, help="integration time in milliseconds (default %default)")
+
+(options, args) = parser.parse_args()
+
+filename = args[0]
+fs = float(args[1])
+coffset = float(args[2])
+prns = util.parse_list_ranges(options.prn)
+doppler_search = util.parse_list_floats(options.doppler_search)
+ms = options.time
+
+# read first portion of file
+
+ms_pad = ms + 5
+n = int(fs*0.001*ms_pad)
+fp = open(filename,"rb")
+x = io.get_samples_complex(fp,n)
+
+# wipe off nominal offset from channel center to Xona X1 carrier
+
+nco.mix(x,-coffset/fs,0)
+
+# resample to 4.096 MHz
+
+fsr = 4096000.0/fs
+h = scipy.signal.firwin(161,1.5e6/(fs/2),window='hann')
+x = scipy.signal.filtfilt(h,[1],x)
+xr = np.interp((1/fsr)*np.arange(ms_pad*4096),np.arange(len(x)),np.real(x))
+xi = np.interp((1/fsr)*np.arange(ms_pad*4096),np.arange(len(x)),np.imag(x))
+x = xr+(1j)*xi
+
+# iterate (in parallel) over PRNs of interest
+
+def worker(p):
+  x,prn = p
+  metric,code,doppler = search(x,prn,doppler_search,ms)
+  return 'prn %3d doppler % 7.1f metric % 5.2f code_offset %6.1f' % (prn,doppler,metric,code)
+
+import multiprocessing as mp
+
+cpus = mp.cpu_count()
+results = mp.Pool(cpus).map(worker, map(lambda prn: (x,prn),prns))
+
+for r in results:
+  print(r)
---
+++ track-xona-x1p.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+
+import optparse
+
+import numpy as np
+
+import gnsstools.xona.x1p as x1p
+import gnsstools.nco as nco
+import gnsstools.io as io
+import gnsstools.discriminator as discriminator
+import gnsstools.util as util
+
+class tracking_state:
+  def __init__(self,fs,prn,code_p,code_f,code_i,carrier_p,carrier_f,carrier_i,mode):
+    self.fs = fs
+    self.prn = prn
+    self.code_p = code_p
+    self.code_f = code_f
+    self.code_i = code_i
+    self.carrier_p = carrier_p
+    self.carrier_f = carrier_f
+    self.carrier_i = carrier_i
+    self.mode = mode
+    self.prompt1 = 0 + 0*(1j)
+    self.carrier_e1 = 0
+    self.code_e1 = 0
+    self.eml = 0
+    self.carrier_cyc = 0
+    self.code_cyc = 0
+
+# tracking loops
+
+def track(x,s):
+  n = len(x)
+  fs = s.fs
+
+  nco.mix(x,-s.carrier_f/fs, s.carrier_p)
+  s.carrier_p = s.carrier_p - n*s.carrier_f/fs
+  t = np.mod(s.carrier_p,1)
+  dcyc = int(round(s.carrier_p-t))
+  s.carrier_cyc += dcyc
+  s.carrier_p = t
+
+  cf = (s.code_f+s.carrier_f/1557.5)/fs
+
+  p_early = x1p.correlate(x, s.prn, 0, s.code_p-0.05, cf, x1p.x1p_code(prn))
+  p_prompt = x1p.correlate(x, s.prn, 0, s.code_p, cf, x1p.x1p_code(prn))
+  p_late = x1p.correlate(x, s.prn, 0, s.code_p+0.05, cf, x1p.x1p_code(prn))
+
+  if s.mode=='FLL_WIDE':
+    fll_k = 3.0
+    a = p_prompt
+    b = s.prompt1
+    e = discriminator.fll_atan(a,b)
+    s.carrier_f = s.carrier_f + fll_k*e
+    s.prompt1 = p_prompt
+  elif s.mode=='FLL_NARROW':
+    fll_k = 0.8
+    a = p_prompt
+    b = s.prompt1
+    e = discriminator.fll_atan(a,b)
+    s.carrier_f = s.carrier_f + fll_k*e
+    s.prompt1 = p_prompt
+  elif s.mode=='PLL':
+#    pll_k1 = 0.1
+#    pll_k2 = 3.5
+    pll_k1 = 0.5
+    pll_k2 = 15
+    e = discriminator.pll_costas(p_prompt)
+    e1 = s.carrier_e1
+    s.carrier_f = s.carrier_f + pll_k1*e + pll_k2*(e-e1)
+    s.carrier_e1 = e
+
+# code loop
+
+  dll_k1 = 0.00002
+  dll_k2 = 0.2
+  s.early = np.absolute(p_early)
+  s.prompt = np.absolute(p_prompt)
+  s.late = np.absolute(p_late)
+  if (s.late+s.early)==0:
+    e = 0
+  else:
+    e = (s.late-s.early)/(s.late+s.early)
+  s.eml = e
+  e1 = s.code_e1
+  s.code_f = s.code_f + dll_k1*e + dll_k2*(e-e1)
+  s.code_e1 = e
+
+  s.code_p = s.code_p + n*cf
+  t = np.mod(s.code_p,x1p.code_length)
+  dcyc = int(round(s.code_p-t))
+  s.code_cyc += dcyc
+  s.code_p = t
+
+  return p_prompt,s
+
+#
+# main program
+#
+
+parser = optparse.OptionParser(usage="""track-xona-x1p.py [options] input_filename sample_rate carrier_offset PRN doppler code_offset
+
+Track Xona X1P signal
+
+Examples:
+  Track with default options:
+    track-xona-x1p.py /dev/stdin 69984000 8567625 0 -21000.0 831.15
+  Track with pure PLL (no FLL intervals at the start) and with a specified carrier phase:
+    track-xona-x1p.py --carrier-phase 0.214 /dev/stdin 69984000 8567625 0 -21000.0 831.15
+
+Arguments:
+  input_filename    input data file, i/q interleaved, 8 bit signed
+  sample_rate       sampling rate in Hz
+  carrier_offset    offset to L1 carrier in Hz (positive or negative)
+  PRN               PRN
+  doppler           Doppler estimate from acquisition
+  code_offset       Code-offset estimate from acquisition""")
+
+parser.disable_interspersed_args()
+
+parser.add_option("--loop-dwells", default="500,500", help="initial time intervals for wide FLL, then narrow FLL, in milliseconds (default %default)")
+parser.add_option("--carrier-phase", help="initial carrier phase in cycles (disables FLL: uses PLL from the start)")
+
+(options, args) = parser.parse_args()
+
+filename = args[0]
+fs = float(args[1])
+coffset = float(args[2])
+prn = int(args[3])
+doppler = float(args[4])
+code_offset = float(args[5])
+loop_dwells = util.parse_list_floats(options.loop_dwells)
+carrier_p = 0.0
+if options.carrier_phase is not None:
+  carrier_p = float(options.carrier_phase)
+  loop_dwells = 0,0
+
+fll_wide_time,fll_narrow_time = loop_dwells
+
+fp = open(filename,"rb")
+
+n = int(fs*0.001*((x1p.code_length-code_offset)/x1p.code_length))  # align with 1 ms code boundary
+x = io.get_samples_complex(fp,n)
+code_offset += n*1000.0*x1p.code_length/fs
+
+s = tracking_state(fs=fs, prn=prn,                    # initialize tracking state
+  code_p=code_offset, code_f=x1p.chip_rate, code_i=0,
+  carrier_p=carrier_p, carrier_f=doppler, carrier_i=0,
+#  mode='FLL_WIDE')
+  mode='PLL')
+
+block = 0
+coffset_phase = 0.0
+samp = 0
+
+while True:
+#  if block>=fll_wide_time:
+#    s.mode = 'FLL_NARROW'
+#  if block>=fll_wide_time+fll_narrow_time:
+#    s.mode = 'PLL'
+
+  if s.code_p<x1p.code_length/2:
+    n = int(fs*0.001*(x1p.code_length-s.code_p)/x1p.code_length)
+  else:
+    n = int(fs*0.001*(2*x1p.code_length-s.code_p)/x1p.code_length)
+
+  x = io.get_samples_complex(fp,n)
+  if x is None:
+    break
+  samp += n
+
+  nco.mix(x,-coffset/fs,coffset_phase)
+  coffset_phase = coffset_phase - n*coffset/fs
+  coffset_phase = np.mod(coffset_phase,1)
+
+  p_prompt,s = track(x,s)
+
+  vars = block, np.real(p_prompt), np.imag(p_prompt), s.carrier_f, s.code_f-x1p.chip_rate, (180/np.pi)*np.angle(p_prompt), s.early, s.prompt, s.late, s.code_cyc, s.code_p, s.carrier_cyc, s.carrier_p, samp
+  print('%d %f %f %f %f %f %f %f %f %d %f %d %f %d' % vars)
+
+  block = block + 1
